dc07613246aaffd81e49ee6aa3366826e17a95da
Majkosss <159123359+Majkosss@users.noreply.github.com>
2024-04-02 17:46:40 +0200

    Add files via upload

---
+++ kalkulacka.py
@@ -0,0 +1,37 @@
+import math
+
+def kalkulacka():
+    print("Vítejte v kalkulačce!")
+    
+    while True:
+        num1 = float(input("Vložte první číslo: "))
+        result = num1
+        while True:
+            operator = input("Vložte operátora (+, -, *, /) nebo 'konec' pro výpočet výsledku: ")
+            
+            if operator == "konec":
+                break
+            else:
+                num2 = float(input("Vložte další číslo: "))
+                
+                if operator == "+":
+                    result += num2
+                elif operator == "-":
+                    result -= num2
+                elif operator == "*":
+                    result *= num2
+                elif operator == "/":
+                    result /= num2
+                else:
+                    print("Tenhle operátor není podporován.")
+                    continue
+        
+        print("Výsledek:", result)
+        
+        choice = input("Chcete pokračovat? (ano/ne): ")
+        if choice.lower() != "ano":
+            break
+    
+    print("Děkuji za používání kalkulačky. - Inspirováno Googlem.")
+
+kalkulacka()
